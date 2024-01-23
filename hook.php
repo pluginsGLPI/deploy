e0978a56686763ae019b1005fed1c75c36013c40
LAUNAY Samuel <107540223+Lainow@users.noreply.github.com>
2024-01-23 16:59:36 +0100

    Fix all headers

--- hook.php
+++ hook.php
@@ -3,7 +3,6 @@
 /**
  * -------------------------------------------------------------------------
  * deploy plugin for GLPI
- * Copyright (C) 2022 by the deploy Development Team.
  * -------------------------------------------------------------------------
  *
  * MIT License
@@ -25,8 +24,11 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- *
- * --------------------------------------------------------------------------
+ * -------------------------------------------------------------------------
+ * @copyright Copyright (C) 2022 by the deploy plugin team.
+ * @license   MIT https://opensource.org/licenses/mit-license.php
+ * @link      https://github.com/pluginsGLPI/deploy
+ * -------------------------------------------------------------------------
  */
 
 function plugin_deploy_install()
